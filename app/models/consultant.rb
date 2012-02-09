2b94110cb7064b46f9393e21540b01f7e4186382
subverse <subverse@gmx.net>
2012-02-09 14:33:38 +0100

    stuff

--- app/models/consultant.rb
+++ app/models/consultant.rb
@@ -1,2 +1,3 @@
 class Consultant < ActiveRecord::Base
+  belongs_to :group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
   belongs_to :account
   has_many :subjects
+  has_many :consultants
 end
